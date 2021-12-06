a60afb5e88328b6cbd44d8c62ee967a18393ae61
caspero-62 <nwaforuchechukwu2007@gmail.com>
2021-12-06 04:00:59 +0100

    calender js implementation

--- js/web3/predictionBid.js
+++ js/web3/predictionBid.js
@@ -1,27 +1,87 @@
+const dummyTableData = [
+    {
+        date: '19-12-2021, 20:45',
+        round: 1,
+        myBid: '$58,900.34',
+        firstWon: '$60,000.45',
+        secondWon: '$59,000.45',
+    },
+    {
+        date: '21-12-2021, 20:45',
+        round: 2,
+        myBid: '-',
+        firstWon: '$160,000.45',
+        secondWon: '$159,000.45',
+    },
+    {
+        date: '21-12-2021, 20:45',
+        round: 3,
+        myBid: '$60,000.45',
+        firstWon: '$260,000.45',
+        secondWon: '$259,000.45',
+    },
+    {
+        date: '21-12-2021, 20:45',
+        round: 4,
+        myBid: '$58,900.34',
+        firstWon: '$60,000.45',
+        secondWon: '$59,000.45',
+    },
+];
+
+const bidHistoryTableBody = document.querySelector(
+	'.bid__history__table .table__body'
+);
+
 /*
 ======= Utility Funcs
 */
+
+const populateBidsHistoryTable = (bids) => {
+    bidHistoryTableBody.innerHTML = '';
+	bids.map((bid, idx) => {
+        const bidRow = `
+            <tr class="table__row">
+                <td class="table__data">${bid.date}</td>
+                <td class="table__data">${bid.round}</td>
+                <td class="table__data">${bid.myBid}</td>
+                <td class="table__data">${bid.firstWon}</td>
+                <td class="table__data">${bid.secondWon}</td>
+                ${idx === 0 ? (
+                    `<td class="table__data buttons" rowspan="4">
+                        <div>
+                            <button>Reclaim $Bid</button>
+                            <button>Go to stake</button>
+                        </div>
+                    </td>
+                    `
+                ) : ''}
+            </tr>
+        `;
+        bidHistoryTableBody.innerHTML += bidRow;
+    });
+};
 
 //filter rows based on showing bids
 const filteredRowsShowBids = (rows, checked) => {
 	return [...rows].filter((row) => {
 		return checked
-			? row.querySelectorAll('td')[2].innerText !== '-'
-			: row.querySelectorAll('td')[2].innerText !== '';
+			? row.myBid !== '-'
+			: row.myBid !== '';
 	});
 };
 
 //filter rows based on dates
 const filteredRowsDate = (rows, date) => {
 	return [...rows].filter((row) =>
-		row.querySelector('td').innerText.includes(date)
+		row.date.includes(date)
 	);
 };
 
 // filter rows based on rounds
 const filteredRowsRounds = (rows, rounds) => {
 	return [...rows].filter(
-		(row) => row.querySelectorAll('td')[1].innerText <= +rounds
+		(row) => row.round <= +rounds
 	);
 };
 
@@ -29,10 +89,7 @@ const filteredRowsRounds = (rows, rounds) => {
 ======= Utility Funcs
 */
 
-const bidHistoryTableBody = document.querySelector(
-	'.bid__history__table .table__body'
-);
-const bidHistoryTableRows = bidHistoryTableBody.querySelectorAll('.table__row');
+populateBidsHistoryTable(dummyTableData);
 
 const calender = document.querySelector('#choose-date');
 const calenderValue = document.querySelector('.search__date .calender__value');
@@ -40,19 +97,18 @@ const calenderValue = document.querySelector('.search__date .calender__value');
 calender.addEventListener('change', () => {
 	let showBids = document.querySelector('#show-bid');
 	let showRoundsOption = document.querySelector('#show-rounds');
-	bidHistoryTableBody.innerHTML = '';
 	let filteredRows;
 	if (calender.value === '') {
 		calenderValue.innerText = 'Search by Date';
-		filteredRows = filteredRowsShowBids(bidHistoryTableRows, showBids.checked);
+		filteredRows = filteredRowsShowBids(dummyTableData, showBids.checked);
 	} else {
 		let formattedDate = calender.value.split('-').reverse().join('-');
 		calenderValue.innerText = formattedDate;
-		filteredRows = filteredRowsDate(bidHistoryTableRows, formattedDate);
+		filteredRows = filteredRowsDate(dummyTableData, formattedDate);
 		filteredRows = filteredRowsShowBids(filteredRows, showBids.checked);
 	}
 	filteredRows = filteredRowsRounds(filteredRows, showRoundsOption.value);
-	filteredRows.map((row) => bidHistoryTableBody.appendChild(row));
+	populateBidsHistoryTable(filteredRows);
 });
 
 const showBids = document.querySelector('#show-bid');
@@ -60,16 +116,15 @@ const showBids = document.querySelector('#show-bid');
 showBids.addEventListener('change', () => {
 	let calenderValue = document.querySelector('.search__date .calender__value');
 	let showRoundsOption = document.querySelector('#show-rounds');
-	bidHistoryTableBody.innerHTML = '';
-	let filteredRows = filteredRowsShowBids(bidHistoryTableRows, showBids.checked);
+	let filteredRows = filteredRowsShowBids(
+		dummyTableData,
+		showBids.checked
+	);
 	if (calenderValue.innerText !== 'Search by Date') {
-		filteredRows = filteredRowsDate(
-			filteredRows,
-			calenderValue.innerText
-		);
+		filteredRows = filteredRowsDate(filteredRows, calenderValue.innerText);
 	}
 	filteredRows = filteredRowsRounds(filteredRows, showRoundsOption.value);
-	filteredRows.map((row) => bidHistoryTableBody.appendChild(row));
+	populateBidsHistoryTable(filteredRows);
 });
 
 const showRoundsOption = document.querySelector('#show-rounds');
@@ -77,14 +132,13 @@ const showRoundsOption = document.querySelector('#show-rounds');
 showRoundsOption.addEventListener('change', () => {
 	let showBids = document.querySelector('#show-bid');
 	let calenderValue = document.querySelector('.search__date .calender__value');
-	bidHistoryTableBody.innerHTML = '';
-	let filteredRows = filteredRowsShowBids(bidHistoryTableRows, showBids.checked);
+	let filteredRows = filteredRowsShowBids(
+		dummyTableData,
+		showBids.checked
+	);
 	if (calenderValue.innerText !== 'Search by Date') {
-		filteredRows = filteredRowsDate(
-			filteredRows,
-			calenderValue.innerText
-		);
+		filteredRows = filteredRowsDate(filteredRows, calenderValue.innerText);
 	}
 	filteredRows = filteredRowsRounds(filteredRows, showRoundsOption.value);
-	filteredRows.map((row) => bidHistoryTableBody.appendChild(row));
+	populateBidsHistoryTable(filteredRows);
 });
